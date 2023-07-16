7d79a680b1551b62cb607890ef6a25b13506f01e
Segoy <segoy.thiel@protonmail.com>
2023-07-16 18:08:39 +0200

    changed pipeline Resource hopefully fixing not pulling git repo

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -40,12 +40,12 @@ aws cloudformation deploy \
 	--no-fail-on-empty-changeset \
 	--capabilities CAPABILITY_NAMED_IAM \
 	--parameter-overrides \
-	EC2InstanceType=$EC2_INSTANCE_TYPE \
-	GitHubOwner=$GH_OWNER \
-	GitHubRepo=$GH_REPO \
-	GitHubBranch=$GH_BRANCH \
-	GitHubPersonalAccessToken=$GH_ACCESS_TOKEN \
-	CodePipelineBucket=$CODEPIPELINE_BUCKET
+		EC2InstanceType=$EC2_INSTANCE_TYPE \
+		GitHubOwner=$GH_OWNER \
+		GitHubRepo=$GH_REPO \
+		GitHubBranch=$GH_BRANCH \
+		GitHubPersonalAccessToken=$GH_ACCESS_TOKEN \
+		CodePipelineBucket=$CODEPIPELINE_BUCKET
 
 # If the deploy succeeded, show the DNS name of the created instance
 if [ $? -eq 0 ]; then
