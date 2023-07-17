e1f5d19432e912533d86e81b4fd2e40180a43a6a
Segoy <segoy.thiel@protonmail.com>
2023-07-17 12:36:52 +0200

    IP Calls in Scripts still unauthorized -> fix with ugly workaround

--- start-service.sh
+++ start-service.sh
@@ -3,8 +3,12 @@ source /home/ec2-user/.bash_profile
 cd /home/ec2-user/app/release
 
 # Query the EC2 metadata service for this instance's region
-REGION=`curl -s http://169.254.169.254/latest/dynamic/instanceidentity/
-document | jq .region -r`
+#REGION=`curl -s http://169.254.169.254/latest/dynamic/instanceidentity/
+#document | jq .region -r`
+#ugly workaround because ip call gets 401
+LONG_REGION=$(ec2-metadata --availability-zone)
+REGION="${LONG_REGION##*:}"
+
 # Query the EC2 metadata service for this instance's instance-id
 export INSTANCE_ID=`less /var/lib/cloud/data/instance-id`
 # Query EC2 describeTags method and pull our the CFN Logical ID for this
