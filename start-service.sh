cb9a49d94800ef84c6c0c83438b10014790bbbae
Segoy <segoy.thiel@protonmail.com>
2023-07-19 07:43:14 +0200

    added http request for region and instance id with token

--- start-service.sh
+++ start-service.sh
@@ -3,14 +3,12 @@ source /home/ec2-user/.bash_profile
 cd /home/ec2-user/app/release
 
 # Query the EC2 metadata service for this instance's region
-#REGION=`curl -s http://169.254.169.254/latest/dynamic/instanceidentity/
-#document | jq .region -r`
-#ugly workaround because ip call gets 401
-LONG_REGION=$(ec2-metadata --availability-zone)
-REGION="${LONG_REGION##*: }"
+#REGION=`curl -s http://169.254.169.254/latest/dynamic/instanceidentity/document | jq .region -r`
+TOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"`
+REGION=`curl -H "X-aws-ec2-metadata-token: $TOKEN" -v http://169.254.169.254/latest/meta-data/placement/region`
 
 # Query the EC2 metadata service for this instance's instance-id
-export INSTANCE_ID=`less /var/lib/cloud/data/instance-id`
+export INSTANCE_ID=`curl -H "X-aws-ec2-metadata-token: $TOKEN" -v http://169.254.169.254/latest/meta-data/instance-id`
 # Query EC2 describeTags method and pull our the CFN Logical ID for this instance
 export STACK_NAME=`aws --region $REGION ec2 describe-tags \
 	--filters "Name=resource-id,Values=${INSTANCE_ID}" \
